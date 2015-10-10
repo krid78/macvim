128f5041142c5bef9f1d41eadbeef91c6a217764
Kazuki Sakamoto <sakamoto@splhack.org>
2015-10-10 00:39:42 -0700

    Reorder methods

--- src/MacVim/MMWindowController.m
+++ src/MacVim/MMWindowController.m
@@ -1221,15 +1221,6 @@
     }
 }
 
-- (void)windowDidExitFullScreen:(NSNotification *)notification
-{
-    if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_10_Max) {
-        // NOTE: On El Capitan, we need to redraw the view when leaving
-        // full-screen by moving the window out from Split View.
-        [vimController sendMessage:BackingPropertiesChangedMsgID data:nil];
-    }
-}
-
 - (void)windowDidFailToEnterFullScreen:(NSWindow *)window
 {
     // NOTE: This message can be called without
@@ -1310,6 +1301,15 @@
         // gets them back into sync.
         fullScreenEnabled = NO;
         [self invFullScreen:self];
+    }
+}
+
+- (void)windowDidExitFullScreen:(NSNotification *)notification
+{
+    if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_10_Max) {
+        // NOTE: On El Capitan, we need to redraw the view when leaving
+        // full-screen by moving the window out from Split View.
+        [vimController sendMessage:BackingPropertiesChangedMsgID data:nil];
     }
 }
 
